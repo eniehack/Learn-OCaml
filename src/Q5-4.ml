d3b4c72c2f2f0c75cd727277e88bfbf275e66619
caffeine <eniehack@outlook.jp>
2019-01-17 20:59:46 +0900

    5章問6を解いた

--- src/Q5-4.ml
+++ src/Q5-4.ml
@@ -16,3 +16,13 @@ let kai_no_kosuu a b c =
 let test4 = kai_no_kosuu 1 2 1 = 1
 let test5 = kai_no_kosuu 1 4 1 = 2
 let test6 = kai_no_kosuu 2 2 1 = 2
+
+(* 目的:二次方程式の解の判別式の解からその二次方程式が虚数解を持つのかを判定する *)
+(* kyosuukai: int -> int -> int -> string *)
+let kyosuukai a b c = 
+    if hanbetsushiki a b c < 0 then "True" else "False"
+
+(* テスト *)
+let test7 = kyosuukai 1 2 1 = "False"
+let test8 = kyosuukai 1 4 1 = "False"
+let test9 = kyosuukai 2 2 1 = "True"
