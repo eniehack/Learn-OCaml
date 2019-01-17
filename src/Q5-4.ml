eb6064e8ef7a6d6b4241971e2c44c4be58dae16a
caffeine <eniehack@outlook.jp>
2019-01-17 20:48:30 +0900

    5章問4,5を解いた

---
+++ src/Q5-4.ml
@@ -0,0 +1,18 @@
+(* 目的:2次方程式の係数3つを与えられたら判別式の解を返す *)
+(* hanbetsushiki: Int->Int->Int->Int *)
+let hanbetsushiki a b c = b * b - 4 * a * c
+
+(* テスト *)
+let test1 = hanbetsushiki 1 2 1 = 0
+let test2 = hanbetsushiki 1 4 1 = 12
+let test3 = hanbetsushiki 2 2 1 = -4
+
+(* 目的:二次方程式の解の判別式の解を与えられたらその二次方程式の解の個数を返す *)
+(* kai_no_kosuu: int -> int -> int -> int *)
+let kai_no_kosuu a b c =
+    if hanbetsushiki a b c = 0 then 1 else 2
+
+(* テスト *)
+let test4 = kai_no_kosuu 1 2 1 = 1
+let test5 = kai_no_kosuu 1 4 1 = 2
+let test6 = kai_no_kosuu 2 2 1 = 2
