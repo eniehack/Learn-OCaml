9aa529254e59d6cb750991907173208a87a915b7
Nakaya <eniehack@outlook.jp>
2023-01-03 03:12:28 +0900

    コード量を削減

--- src/Q10-3.ml
+++ src/Q10-3.ml
@@ -4,15 +4,9 @@ type gakusei_t = {
     score : string;
 }
 
-let lst1 = []
-let lst2 = [
-    {name = "asai"; point = 70; score = "B"};
-]
-let lst3 = [
-    {name = "yoshida"; point = 85; score = "A"};
-    {name = "asai"; point = 70; score = "B"};
-    {name = "kaneko"; point = 80; score = "A"};
-]
+let gakusei1 = {name = "asai"; point = 70; score = "B"}
+let gakusei2 = {name = "yoshida"; point = 85; score = "A"}
+let gakusei3 = {name = "kaneko"; point = 80; score = "A"}
 
 (* 目的: gakusei_tの要素を第2引数のリストへ成績の昇順になるようソートする *)
 (* insert : gakusei_t -> gakusei_t list -> gakusei_t list *)
@@ -23,23 +17,9 @@ let rec insert gakusei lst = match lst with
       then gakusei :: first :: rest
       else first :: (insert gakusei rest)
 
-let test1 = insert {name = "asai"; point = 70; score = "B"} [] = lst2
-let test2 = insert {name = "asai"; point = 70; score = "B"} [
-    {name = "kaneko"; point = 80; score = "A"};
-    {name = "yoshida"; point = 85; score = "A"};
-] = [
-    {name = "asai"; point = 70; score = "B"};
-    {name = "kaneko"; point = 80; score = "A"};
-    {name = "yoshida"; point = 85; score = "A"};
-]
-let test3 = insert {name = "yoshida"; point = 85; score = "A"} [
-    {name = "asai"; point = 70; score = "B"};
-    {name = "kaneko"; point = 80; score = "A"};
-] = [
-    {name = "asai"; point = 70; score = "B"};
-    {name = "kaneko"; point = 80; score = "A"};
-    {name = "yoshida"; point = 85; score = "A"};
-]
+let test1 = insert gakusei1 [] = [gakusei1]
+let test2 = insert gakusei1 [gakusei3; gakusei2;] = [gakusei1; gakusei3; gakusei2]
+let test3 = insert gakusei2 [gakusei1; gakusei3] = [gakusei1; gakusei3; gakusei2]
 
 (* 目的: ソートされていないリストの要素を1つずつ取り出し、ソート済みリストに挿入する *)
 (* gakusei_sort_aux : gakusei_t list -> gakusei_t list -> gakusei_t list *)
@@ -48,13 +28,9 @@ let rec gakusei_sort_aux sorted lst = match lst with
   | first :: rest -> gakusei_sort_aux (insert first sorted) rest
 
 let test4 = gakusei_sort_aux [] [] = []
-let test5 = gakusei_sort_aux lst2 [] = lst2
-let test6 = gakusei_sort_aux [] lst2 = lst2
-let test7 = gakusei_sort_aux [] lst3 = [
-    {name = "asai"; point = 70; score = "B"};
-    {name = "kaneko"; point = 80; score = "A"};
-    {name = "yoshida"; point = 85; score = "A"};
-]
+let test5 = gakusei_sort_aux [gakusei2] [] = [gakusei2]
+let test6 = gakusei_sort_aux [] [gakusei2] = [gakusei2]
+let test7 = gakusei_sort_aux [] [gakusei2; gakusei1; gakusei3] = [gakusei1; gakusei3; gakusei2]
 
 (* 目的: gakusei_tのリストを成績の昇順にソートする *)
 (* gakusei_sort : gakusei_t list -> gakusei_t list *)
@@ -62,10 +38,6 @@ let gakusei_sort lst = match lst with
     [] -> []
   | first :: rest -> gakusei_sort_aux [first] rest
 
-let test8 = gakusei_sort lst1 = []
-let test9 = gakusei_sort lst2 = lst2
-let test10 = gakusei_sort lst3 = [
-    {name = "asai"; point = 70; score = "B"};
-    {name = "kaneko"; point = 80; score = "A"};
-    {name = "yoshida"; point = 85; score = "A"};
-]
+let test8 = gakusei_sort [] = []
+let test9 = gakusei_sort [gakusei2] = [gakusei2]
+let test10 = gakusei_sort [gakusei2; gakusei1; gakusei3] = [gakusei1; gakusei3; gakusei2]
